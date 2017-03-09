5ad109feb07edb764ae0e68d117fc60efd6d133b
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2017-03-09 04:28:05 -0500

    First spring

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,78 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "us-cdbr-iron-east-03.cleardb.net",
+  user: "b2fcba8e2a2458",
+  password: "3e95e7215b251ef",
+  database: "heroku_486681e5e61db3b"
+});
+
+/**
+ *Code to make the connection to the database
+ *
+ **/
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+/**
+ *Code to Ping the database and keep connection awake
+ *
+ **/
+setInterval(function () {
+    con.query('SELECT 1');
+}, 5000);
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/code', function(req, res){
-	res.send('Hello code');
+router.get('/getShoppingList', function(req, res){
+	con.query('SELECT * FROM shoppingcart', function(err, rows){
+		if (err) {
+			res.send(err);
+		} else {
+			console.log(rows);
+			res.render('shoppingKart', {
+				shoppingList: rows
+			});
+		}
+	});
+});
+
+router.get('/getResults', function(req, res) {
+	res.render('searchResults');
+});
+
+router.post('/deleteItem/:item', function(req, res){
+	console.log(req.params.item);
+	con.query('DELETE FROM shoppingcart WHERE ItemName = ?', req.params.item, function(err, result){
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('/getShoppingList');
+		}
+	});
+});
+
+router.post('/addItem', function(req, res){
+	console.log(req.body);
+	con.query('INSERT INTO shoppingcart SET ?', req.body, function(err, result){
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('/getShoppingList');
+		}
+	});
 });
 
 module.exports = router;
