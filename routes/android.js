d76c76e479b946ef8c5a087b9689bdd780933e1a
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2017-04-08 14:24:15 -0400

    Added body parameters

--- routes/android.js
+++ routes/android.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
-	console.log(req);
+	console.log(req.body);
 	res.send("Hello I am a response");
 });
 
