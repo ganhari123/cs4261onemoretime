3fd9d4fe8db4c9b3b4696c95c9df23fc164d4678
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2017-04-08 13:49:25 -0400

    printing out request

--- routes/android.js
+++ routes/android.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
-	console.log(req.body);
+	console.log(req);
 	res.send("Hello I am a response");
 });
 
