a6c2fb07adf1fda604624df348016b95c34915e4
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2017-04-08 14:48:36 -0400

    Added mysql login code to android portion

--- routes/android.js
+++ routes/android.js
@@ -1,10 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "us-cdbr-iron-east-03.cleardb.net",
+  user: "b2fcba8e2a2458",
+  password: "aceb39dabff4f8c",
+  database: "heroku_486681e5e61db3b",
+  multipleStatements: true
+});
+
+/**
+ *Code to make the connection to the database
+ *
+ **/
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+/**
+ *Code to Ping the database and keep connection awake
+ *
+ **/
+setInterval(function () {
+    con.query('SELECT 1');
+}, 5000);
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
 	console.log(req.query);
-	res.send("Hello I am a response");
+	con.query('SELECT * FROM users WHERE username = ?', req.query.username, function(err, rows){
+            if (err) {
+                console.log('error');
+            } else {
+                if (rows.length === 0) {
+                    res.send('either username or password is wrong');
+                } else if (rows[0].password !== req.query.password) {
+                    res.send('either username or password is wrong');
+                } else {
+                	res.send('login successful');
+                }
+                
+            }
+        });
 });
 
 module.exports = router;
